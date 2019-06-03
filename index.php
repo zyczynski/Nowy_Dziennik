ccf981c95ae7e2966d20af7d614932e5966bb7c1
Bartek <zyczynski7@gmail.com>
2019-06-03 18:05:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 
 include "Uczen.php";
+include "Nauczyciel.php";
 
-$uczen = new Uczen(30, "Wacek", "Waclawski", "2b");
+$uczen1 = new Uczen(30, "Wacek", "Waclawski", "2b");
+$nauczeciel1 = new Nauczyciel("Franca", "Glupia", "matematyka", "2b");
 
 
-echo $uczen->getImie();
+echo "Najlepszy uczeń to " .$uczen1->getImie();
+echo "<br>";
 
+echo "Najgorszy nauczyciel to " . $nauczeciel1->getNazwisko() . " " . $nauczeciel1->getImie();
